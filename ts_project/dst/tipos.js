df5e9f5bfca0801cb139f020f692cdff90ec4185
Leo Moreno <leon.moreno@pm.me>
2023-05-11 15:09:24 -0400

    dejando clean

---
+++ ts_project/dst/tipos.js
@@ -0,0 +1,8 @@
+"use strict";
+// Numeros
+let miEdad = 41;
+// string
+let miNombre = "Leonardo";
+let myNum;
+myNum = null;
+myNum = 42;
---
+++ ts_project/src/tipos.ts
@@ -0,0 +1,15 @@
+
+
+// Numeros
+let miEdad: number = 41;
+
+// string
+
+let miNombre: string =  "Leonardo";
+
+
+let myNum: null | number;
+
+myNum = null;
+
+myNum = 42;
